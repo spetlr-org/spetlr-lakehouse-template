7e9418e44916322b21b1e6094281876c411ea262
Simon Heisterkamp <she@delegate.dk>
2023-06-26 15:18:41 +0200

    fix pipeline again

--- .github/deploy/steps/20-deploy-bicep.ps1
+++ .github/deploy/steps/20-deploy-bicep.ps1
@@ -3,16 +3,16 @@ Write-Host "  Deploying ressources using Bicep..." -ForegroundColor Yellow
 
 
 $output = az deployment sub create `
-  --location $location `
-  --template-file $repoRoot/.github/deploy-bicep/main.bicep `
+  --location "$location" `
+  --template-file "$repoRoot/.github/deploy-bicep/main.bicep" `
   --parameters `
-      location=$location `
-      keyVaultName=$keyVaultName `
-      resourceTags=$resourceTags `
-      resourceGroupName=$resourceGroupName `
-      databricksName=$databricksName `
-      dataLakeName=$dataLakeName `
-      datalakeContainers=$dataLakeContainersJson
+      location="$location" `
+      keyVaultName="$keyVaultName" `
+      resourceTags="$resourceTags" `
+      resourceGroupName="$resourceGroupName" `
+      databricksName="$databricksName" `
+      dataLakeName="$dataLakeName" `
+      datalakeContainers="$dataLakeContainersJson"
 
 
 Throw-WhenError -output $output
