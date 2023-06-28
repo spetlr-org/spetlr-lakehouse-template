2d5a707329946ebbe5d0a538c977296c43b79450
Lau Johansson <ljo@delegate.dk>
2023-06-28 14:16:40 +0200

    feat: restructure

--- demo/src/dataplatform/etl/__init__.py
+++
@@ -1,9 +0,0 @@
-from .demo_elt import DemoEtl
-from .demo_params import DemoParams
-from .my_filter_transformer import MyFilterTransformer
-
-__all__ = [
-    "DemoParams",
-    "DemoEtl",
-    "MyFilterTransformer",
-]
