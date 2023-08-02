097ab322253caecd6a598062c6da21dab480e2a4
FarBo <farzadb58@gmail.com>
2023-08-02 12:56:44 +0200

    Silver and gold etl

---
+++ src/dataplatform/etl/nyc_tlc/gold/nyc_tlc_gold_orchestrator.py
@@ -0,0 +1,22 @@
+from spetlr.etl import Orchestrator
+from spetlr.etl.loaders.simple_loader import SimpleLoader
+
+from dataplatform.etl.nyc_tlc.gold.nyc_tlc_gold_parameters import NycTlcGoldParameters
+from dataplatform.etl.nyc_tlc.gold.nyc_tlc_gold_transformer import NycTlcGoldTransfomer
+
+
+class NycTlcSilverOrchestrator(Orchestrator):
+    def __init__(self, params: NycTlcGoldParameters = None):
+        super().__init__()
+
+        self.params = params or NycTlcGoldParameters()
+
+        self.extract_from(self.params.dh_source)
+
+        self.transform_with(NycTlcGoldTransfomer)
+
+        self.load_into(
+            SimpleLoader(
+                self.params.dh_target,
+            )
+        )
---
+++ src/dataplatform/etl/nyc_tlc/silver/nyc_tlc_silver_transformer.py
@@ -0,0 +1,35 @@
+import pyspark.sql.functions as f
+from pyspark.sql import DataFrame
+from spetlr.etl import Transformer
+
+from dataplatform.etl.nyc_tlc.silver.nyc_tlc_silver_parameters import (
+    NycTlcSilverParameters,
+)
+
+
+class NycTlcSilverTransfomer(Transformer):
+    def __init__(self, params: NycTlcSilverParameters):
+        super().__init__()
+        self.params = params or NycTlcSilverParameters()
+
+    def process(self, df: DataFrame) -> DataFrame:
+        df = df.withColumn(
+            "paymentType",
+            f.when(f.col("paymentType") == 1, "Credit")
+            .when(f.col("paymentType") == 2, "Cash")
+            .when(f.col("paymentType") == 3, "No charge")
+            .when(f.col("paymentType") == 4, "Dispute")
+            .when(f.col("paymentType") == 5, "Unknown")
+            .when(f.col("paymentType") == 6, "Voided trip"),
+        )
+
+        df_final = df.select(
+            f.col("vendorID"),
+            f.col("passengerCount"),
+            f.col("tripDistance"),
+            f.col("paymentType").cast("string"),
+            f.col("tipAmount"),
+            f.col("totalAmount"),
+        )
+
+        return df_final
