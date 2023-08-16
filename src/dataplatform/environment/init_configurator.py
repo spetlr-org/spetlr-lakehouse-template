53d55c9db785bf772f2fe7a92e46eaeacfbe9f65
FarBo <farzadb58@gmail.com>
2023-08-16 14:33:55 +0200

    Add table_names to configurator

--- src/dataplatform/environment/init_configurator.py
+++ src/dataplatform/environment/init_configurator.py
@@ -1,12 +1,13 @@
 from spetlr import Configurator
 
-from dataplatform.environment import config
+from dataplatform.environment import config, table_names
 from dataplatform.environment.databases import bronze, gold, silver
 
 
 def init_configurator():
     c = Configurator()
     c.add_resource_path(config)
+    c.add_resource_path(table_names)
     c.add_sql_resource_path(bronze)
     c.add_sql_resource_path(silver)
     c.add_sql_resource_path(gold)
