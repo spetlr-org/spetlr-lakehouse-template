1f4b615b1105f6f45af01bf446b28e4a51718ff5
FarBo <farzadb58@gmail.com>
2023-08-21 10:37:32 +0200

    import init_configurator correctly in main

--- src/dataplatform/etl/nyc_tlc/gold/main.py
+++ src/dataplatform/etl/nyc_tlc/gold/main.py
@@ -1,4 +1,4 @@
-from dataplatform.environment import init_configurator
+from dataplatform.environment.init_configurator import init_configurator
 from dataplatform.environment.setup_environment import setup_environment
 from dataplatform.etl.nyc_tlc.gold.nyc_tlc_gold_orchestrator import (
     NycTlcGoldOrchestrator,
