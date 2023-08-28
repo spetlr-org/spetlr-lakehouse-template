31476edb3d4d7fb18674b0bbeeb333efac4e0ec2
FarBo <farzadb58@gmail.com>
2023-08-28 16:32:03 +0200

    Test source on cluster

--- test/cluster/etl/source/test_source_access.py
+++ test/cluster/etl/source/test_source_access.py
@@ -1,2 +1,24 @@
-# Ensure that the storage can be accessed
-# dbtuils.fs.ls()
+import unittest
+
+from spetlr.spark import Spark
+
+from dataplatform.etl.nyc_tlc.bronze.nyc_tlc_bronze_parameters import (
+    NycTlcBronzeParameters,
+)
+
+
+class TestSource(unittest.TestCase):
+    @classmethod
+    def setup_method(cls):
+        cls.params = NycTlcBronzeParameters()
+        cls.path = cls.params.nyc_tlc_path
+        cls.sc = Spark.get().sparkContext
+        cls.fs = cls.sc._jvm.org.apache.hadoop.fs.FileSystem.get(
+            cls.sc._jsc.hadoopConfiguration()
+        )
+
+    def test_01_source_path_exists(self):
+        """
+        This method tests if the csv source path exists in the abfs file system
+        """
+        assert self.fs.exists(self.sc._jvm.org.apache.hadoop.fs.Path(self.path))
